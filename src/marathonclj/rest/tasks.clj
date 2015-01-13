941938ef059fd0f6655fbca2ab57ac4824daf5d2
Halit Olali <halitolali@gmail.com>
2015-01-13 22:23:28 +0100

    tasks api

--- src/marathonclj/rest/tasks.clj
+++ src/marathonclj/rest/tasks.clj
@@ -1,1 +1,11 @@
-(ns marathonclj.rest.tasks)
+(ns marathonclj.rest.tasks
+  (:require [marathonclj.rest :as r]
+            :reload-all)
+  (:import marathonclj.rest.Connection
+           clojure.lang.IPersistentMap))
+
+
+(defn get-tasks
+  "GET /v2/tasks"
+  [^Connection conn]
+  (r/get conn (r/url-with-path conn "v2" "tasks")))
