812448f78b6a40f597ed8871db54ff8ed180440b
Halit Olali <halitolali@gmail.com>
2015-08-09 09:58:24 +0200

    readme, release

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject marathonclj "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Marathon Rest Client for Clojure "
+  :url "https://github.com/codemomentum/marathonclj"
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [cheshire "5.3.1"]
                  [org.clojure/data.json "0.2.5"]
