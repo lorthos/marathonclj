42379b38554bd4f8ba5ba65ff73db206956bf2e0
Halit Olali <halitolali@gmail.com>
2015-08-10 22:05:56 +0200

    docker example

--- test/marathonclj/rest/usage.clj
+++ test/marathonclj/rest/usage.clj
@@ -3,25 +3,31 @@
   (:require [marathonclj.rest.apps :as apps]
             [clojure.data.json :as json]
             [marathonclj.rest.info :as info]
-            [marathonclj.rest.deployments :as deployments])
+            [marathonclj.rest.deployments :as deployments]
+            [clojure.pprint :as pp])
   (:import marathonclj.rest.Connection))
 
 
 (comment
 
-  (def play-app
-    (json/read-str (slurp "resources/play-hello.json") :key-fn keyword))
+  (def docker-container
+    (json/read-str (slurp "resources/docker_example.json") :key-fn keyword))
+
 
   (def conn (Connection. "http://10.141.141.10:8080" {}))
 
-  (apps/create-app conn play-app)
+  (apps/create-app conn docker-container)
 
   (deployments/get-deployments conn)
 
   (apps/get-apps conn)
 
+  (pp/pprint (->> (apps/get-apps conn)
+                  :apps
+                  first))
+
   (info/server-info conn)
 
-  (apps/delete-app conn "hello")
+  (apps/delete-app conn "instance1")
 
   )
