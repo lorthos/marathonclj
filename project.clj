8acf2c2635dc5765d724a95c0769f70dbffd5d89
Halit Olali <halitolali@gmail.com>
2015-08-14 23:54:57 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marathonclj "0.1.1"
+(defproject marathonclj "0.1.2-SNAPSHOT"
   :description "Marathon Rest Client for Clojure "
   :url "https://github.com/codemomentum/marathonclj"
   :dependencies [[org.clojure/clojure "1.7.0"]
